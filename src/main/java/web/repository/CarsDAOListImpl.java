7873b21b8caaf36dc0650232b498a5022dd273e2
Nikita <worldofstyks@gmail.com>
2024-12-10 01:20:02 +0300

    constants renamed

--- src/main/java/web/repository/CarsDAOListImpl.java
+++ src/main/java/web/repository/CarsDAOListImpl.java
@@ -8,14 +8,14 @@ import java.util.List;
 
 @Repository
 public class CarsDAOListImpl implements CarsDAO {
-    private static final Car car1 = new Car("Lada", "Priora", "XTA98743985");
-    private static final Car car2 = new Car("Tesla", "Cybertruck", "SHT40293852");
-    private static final Car car3 = new Car("Audi", "Rs6", "BST293487534");
-    private static final Car car4 = new Car("Nefaz", "5299-40-57", "AAAAVT08U5");
-    private static final Car car5 = new Car("GAZelle", "Next", "MRS564735675");
-    private static final List<Car> cars = Arrays.asList(car1, car2, car3, car4, car5);
+    private static final Car CAR_1 = new Car("Lada", "Priora", "XTA98743985");
+    private static final Car CAR_2 = new Car("Tesla", "Cybertruck", "SHT40293852");
+    private static final Car CAR_3 = new Car("Audi", "Rs6", "BST293487534");
+    private static final Car CAR_4 = new Car("Nefaz", "5299-40-57", "AAAAVT08U5");
+    private static final Car CAR_5 = new Car("GAZelle", "Next", "MRS564735675");
+    private static final List<Car> CARS = Arrays.asList(CAR_1, CAR_2, CAR_3, CAR_4, CAR_5);
 
     public List<Car> getCars() {
-        return List.copyOf(cars);
+        return List.copyOf(CARS);
     }
 }
